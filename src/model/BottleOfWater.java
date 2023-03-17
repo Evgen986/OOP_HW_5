1c33189217a9454f05e13b62d7fc649c80a28b1c
Evgen986 <Evgen986@mail.ru>
2023-03-17 17:21:19 +0300

    initial commit

---
+++ src/model/BottleOfWater.java
@@ -0,0 +1,15 @@
+package model;
+
+public class BottleOfWater extends Drinkables {
+    /**
+     * Перегруженный конструктор класса Бутилированных напитков
+     *
+     * @param name   Наименование напитка
+     * @param price  Стоимость напитка
+     * @param volume Объем напитка
+     */
+    public BottleOfWater(String name, float price, Double volume) {
+        super(name, price, volume);
+    }
+
+}
---
+++ src/service/UserService.java
@@ -0,0 +1,8 @@
+package service;
+
+import view.ConsoleUI;
+
+public interface UserService {
+    void start();
+
+}
